cda95dda062601591a94c0c6c2fe1ce429ec06fb
tusharmadan56 <tusharmadan56@gmail.com>
2026-05-02 19:22:46 +0530

    prompt_template

---
+++ chatbot/prompt_template.py
@@ -0,0 +1,20 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite", temperature=0.7)
+
+# detailed way
+template2 = PromptTemplate(
+    template='Greet this person in 5 languages. The name of the person is {name}',
+    input_variables=['name']
+)
+
+# fill the values of the placeholders
+prompt = template2.invoke({'name':'tushar'})
+
+result = model.invoke(prompt)
+
+print(result.content)
